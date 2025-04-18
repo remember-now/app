e480e5f461d05c529d9f45c13ee524a44ba5dabf
jake158 <kulikovpavel2801@gmail.com>
2025-04-17 22:23:27 -0600

    chore: set up eslint, prettier, husky hook

--- src/App.tsx
+++ src/App.tsx
@@ -31,9 +31,7 @@ function App() {
         </p>
       </div>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 
       <PWABadge />
     </>
